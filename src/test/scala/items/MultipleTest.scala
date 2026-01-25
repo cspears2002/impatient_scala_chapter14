812a3186cb87040df06593e8db418d8720e51935
Christopher Spears <cspears2002@gmail.com>
2026-01-24 16:05:33 -0800

    Add test for different types of Items

--- src/test/scala/items/MultipleTest.scala
+++ src/test/scala/items/MultipleTest.scala
@@ -1,5 +1,19 @@
 package items
 
-class MultipleTest {
+import org.scalatest.funsuite.AnyFunSuite
 
+class MultipleTest extends AnyFunSuite{
+  test("The price of a Multiple with a SimpleItem") {
+    val toaster = SimpleItem(29.95, "Blackwell Toaster")
+    val manyToasters = Multiple(10, toaster)
+    assert(manyToasters.price == 29.95 * 10)
+  }
+  test("The price of a Multiple with a Bundle") {
+    val blackWell = SimpleItem(29.95, "Blackwell Toaster")
+    val whiteWell = SimpleItem(35, "Whitewell Toaster")
+    val toasterBundle = Bundle("Two types of toasters")
+    toasterBundle.itemsArrayBuff.addOne(blackWell)
+    toasterBundle.itemsArrayBuff.addOne(whiteWell)
+    assert(toasterBundle.price == blackWell.price + whiteWell.price)
+  }
 }
