13f4925c5864bef230feb5ac14b339800714a717
Christopher Spears <cspears2002@gmail.com>
2026-03-15 14:25:41 -0700

    Add comments

--- src/main/scala/list/MyList.scala
+++ src/main/scala/list/MyList.scala
@@ -5,8 +5,12 @@ case class MyList[T](list: List[T]):
   def nonEmpty: Boolean = list.nonEmpty
   def head: Option[T] = list.headOption
   def tail: List[T] = list.drop(1)
+  def length: Int = list.length
+
+  // Prepend element
   def ::(elem: T): MyList[T] = MyList(elem::list)
   def ::(elem: MyList[T]): MyList[Any] = MyList(elem::list)
-  def length: Int = list.length
+
+  // Append element
   def :+(elem: T): MyList[T] = MyList(list :+ elem)
   def ++(elem: MyList[T]): MyList[T] = MyList(list ++ elem.list)
